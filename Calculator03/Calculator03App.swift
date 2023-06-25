494623ec528cb2eac79068f0098239979f590cd9
JavierMachucaA <javier.machuca.a@gmail.com>
2023-06-24 22:11:37 -0400

    Initial Commit

---
+++ Calculator03/Calculator03App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator03App.swift
+//  Calculator03
+//
+//  Created by javier machuca on 24-06-23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
